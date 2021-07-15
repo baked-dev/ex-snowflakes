7048695e6f980d35de92743e063b1541c67b6c7d
xhalfbakedguy <tristancilltseinleben@gmail.com>
2021-07-15 16:56:48 +0200

    updated tests to include golang snowflakes

--- test/snowflakes_test.exs
+++ test/snowflakes_test.exs
@@ -20,10 +20,22 @@ defmodule SnowflakesTest do
   test "snowflakes from js" do
     {:ok, pid} = Snowflakes.start_link("test_signing_key", 1023)
 
-    res = "test_parent_d50f0c52c8d0268bef7f0fc03004"
-    res2 = "test_child_5545f08c032f18fc0fd6beb63f07f85f2b0c90f585"
+    res = "test_parent_c6df4ae7a44744cc438fef30c091"
+    res2 = "test_child_761cf03a0e7ff4fc7344ced40370f42f740a10fb26"
     res3 = GenServer.call(pid, {:gen_parent, res2, "test_parent"})
-    res4 = "test_nested_child_0585ef0fac0ce2f268f880f0e6b64b6b8f0faf8f9f2fa0c0d0f0b854"
+    res4 = "test_nested_child_c626af0f9a0ae7f704f4173764d48d4ce7732f4f6f7ff0a060f0f361"
+    res5 = GenServer.call(pid, {:gen_parent, res4, "test_child"})
+    res6 = GenServer.call(pid, {:gen_parent, res5, "test_parent"})
+    assert res5 == res2 and res6 == res
+  end
+
+  test "snowflakes from golang" do
+    {:ok, pid} = Snowflakes.start_link("test_signing_key", 1023)
+
+    res = "test_parent_d66f4a050ab53cf2837fdfb0c0c0"
+    res2 = "test_child_f609f0ea0b5f8afe535c2b2c835bfa7f5c0ad0ff16"
+    res3 = GenServer.call(pid, {:gen_parent, res2, "test_parent"})
+    res4 = "test_nested_child_c616bf0f9a0ac5f57afae5358c2c42c2b3531faf7f5f20a0d0f09260"
     res5 = GenServer.call(pid, {:gen_parent, res4, "test_child"})
     res6 = GenServer.call(pid, {:gen_parent, res5, "test_parent"})
     assert res5 == res2 and res6 == res
